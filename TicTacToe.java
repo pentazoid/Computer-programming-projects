54fe61c27f0f94e7c78ee6989cf82a60347f4ba0
noblegas <larma81087@gmail.com>
2017-02-06 13:27:06 -0500

    TicTacToe Project

---
+++ TicTacToe.java
@@ -0,0 +1,148 @@
+import java.awt.Container;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+
+public class TicTacToe extends JFrame  {
+	private Container pane;
+	private String currentPlayer;
+	private JButton[][] board;
+	private boolean hasWinner;
+	private JMenuBar menuBar;
+	private JMenu menu;
+	private JMenuItem newGame;
+	private JMenuItem quit;
+	public TicTacToe(){
+		
+		super();
+		pane =getContentPane();
+		pane.setLayout(new GridLayout(3,3));
+		setTitle("Tic Tac Toe");
+		setSize(600,600);
+		setResizable(false);
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		setVisible(true);
+		currentPlayer="x";
+		board =new JButton[3][3];
+		hasWinner=false;
+		initializeBoard();
+		initializeMenuBar();
+		
+	}
+	
+	private void initializeMenuBar(){
+		menuBar=new JMenuBar();
+		menu=new JMenu("File");
+		newGame=new JMenuItem(" New GAme");
+		quit=new JMenuItem(" quit");
+		
+		quit.addActionListener(new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+			   System.exit(0);
+				
+			}
+			
+		});
+		
+		newGame.addActionListener(new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				resetBoard();
+				
+			}
+			
+		
+			
+		});
+		
+		menu.add(newGame);
+		menu.add(quit);
+		menuBar.add(menu);
+		setJMenuBar(menuBar);
+		
+	}
+	
+	private void resetBoard(){
+		
+		currentPlayer="x";
+		hasWinner=false;
+		for(int i=0; i<3; i++){
+			for(int j=0; j<3; j++){
+				board[i][j].setText("");
+			}
+		}
+		
+	}
+	
+	private void initializeBoard(){
+		for(int i=0; i<3; i++){
+			for(int j=0; j<3; j++){
+				JButton btn=new JButton("");
+				btn.setFont(new Font(Font.SANS_SERIF,Font.BOLD,100));
+				
+				board[i][j]=btn;
+				
+				btn.addActionListener(new ActionListener(){
+
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						if(((JButton)e.getSource()).getText().equals("")&&
+					   hasWinner==false){
+							
+							btn.setText(currentPlayer);
+							hasWinner();
+							togglePlayer();
+
+							
+						}
+						
+					}
+					
+				});
+				
+				pane.add(btn);
+				
+			}
+		}
+	}
+	
+	private void togglePlayer(){
+		if(currentPlayer.equals("x")){
+			currentPlayer="o";
+		}else{
+			currentPlayer="x";
+		}
+	}
+	
+	private void hasWinner(){
+		if(board[0][0].getText().equals(currentPlayer)&&board[1][0].equals(currentPlayer)&&board[2][0].equals(currentPlayer)){
+			JOptionPane.showMessageDialog(null, "Player"+currentPlayer+"has winner");
+			hasWinner=true;
+		}
+		
+		else if(board[0][1].getText().equals(currentPlayer)&&board[1][1].equals(currentPlayer)&&board[2][1].equals(currentPlayer)){
+			JOptionPane.showMessageDialog(null, "Player"+currentPlayer+"has winner");
+			hasWinner=true;
+		}
+		else if(board[0][2].getText().equals(currentPlayer)&&board[1][2].equals(currentPlayer)&&board[2][2].equals(currentPlayer)){
+			JOptionPane.showMessageDialog(null, "Player"+currentPlayer+"has winner");
+			hasWinner=true;
+		}
+		
+	}
+	
+	
+     
+
+}
